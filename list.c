a4e6fb42adb4d5deb5e25589090f554220ba59c1
andaihn <72494054+andaihn-ai@users.noreply.github.com>
2021-02-09 15:32:46 +0900

    Add files via upload

---
+++ list.c
@@ -0,0 +1,115 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static Node *createNode(int data, const Node *next)
+{
+    Node *ptr = malloc(sizeof(Node));
+    assert(ptr );
+
+    ptr->data = data;
+    ptr->next = (Node *)next;
+
+}
+void initList(List *pList)
+{
+    /*
+    pList->ptr = malloc(sizeof(Node));
+    assert(pList->ptr);
+
+    pList->ptr->next = NULL;
+    */
+    pList->ptr = createNode(-1,NULL);
+}
+
+void cleanUpList(List *pList)
+{
+    Node *ptr = pList->ptr;
+    while(ptr)
+    {
+        Node* tmp = ptr;
+        ptr = ptr->next;
+        free(tmp);
+    }
+    pList->ptr = NULL;
+}
+
+void insertFirstNode(List *pList, int data)
+{
+    /*
+    Node *ptr = malloc(sizeof(Node));
+    assert(ptr );
+
+    ptr->data = data;
+    ptr->next = pList->ptr->next;
+    pList->ptr->next = ptr;
+    */
+    pList->ptr->next = createNode(data,pList->ptr->next);
+
+}
+
+void insertNode(List *pList, int prevData, int data)
+{
+    /*해당하는 prevData 가 있으면 뒤에 삽입*/
+    Node *ptr = pList->ptr->next;
+
+    while(ptr ){
+        if (ptr->data == prevData)
+            break;
+        ptr = ptr->next;
+    }
+
+    if(ptr ){
+        /*
+        Node *tmp = malloc(sizeof(Node));
+        assert(ptr );
+
+        tmp->data = data;
+        tmp->next = ptr->next;
+        ptr->next = tmp;
+        */
+        ptr->next = createNode(data,ptr->next);
+    }
+}
+
+void deleteNode(List *pList, int data)
+{
+    Node *prevPtr = pList->ptr;
+    Node *ptr = pList->ptr->next;
+
+    while (ptr )
+    {
+        /* code */
+        if(ptr->data == data)
+
+            break;
+
+        ptr = ptr->next;
+        prevPtr = prevPtr->next;
+    }
+    if(ptr )
+    {
+        prevPtr->next = ptr->next;
+        free(ptr);
+    }
+
+}
+
+void printList(const List *pList)
+{
+    Node *ptr;
+    if(pList->ptr)
+        ptr = pList->ptr->next;
+    else
+        ptr = NULL;
+
+    printf("[");
+    while(ptr ){
+
+        printf("%d", ptr->data);
+        printf((ptr->next )?", ":"");
+        ptr = ptr->next;
+    }
+    printf("]\n");
+}
